30858bdee63f6d848626605323b2aeb5a7652e3f
Garett Daly <garett@sdeuropean.co.nz>
2025-05-20 16:44:01 +1200

    updated default images

--- ToolManagementAppV2/Utilities/NullToDefaultImageConverter.cs
+++ ToolManagementAppV2/Utilities/NullToDefaultImageConverter.cs
@@ -8,44 +8,56 @@ namespace ToolManagementAppV2.Utilities
 {
     public class NullToDefaultImageConverter : IValueConverter
     {
-        private static BitmapImage _defaultImage;
+        private static BitmapImage _defaultUser;
+        private static BitmapImage _defaultTool;
+        private static BitmapImage _defaultLogo;
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is BitmapImage bitmap)
-                return bitmap;
+            // If we've already got an actual BitmapImage, just return it
+            if (value is BitmapImage bmp) return bmp;
 
-            string path = value as string;
-            if (!string.IsNullOrEmpty(path))
+            // If it's a path, try loading it
+            if (value is string path && !string.IsNullOrEmpty(path))
             {
-                try
-                {
-                    return new BitmapImage(new Uri(path, UriKind.Absolute));
-                }
-                catch { }
+                try { return new BitmapImage(new Uri(path, UriKind.Absolute)); }
+                catch { /* fall‐through to default */ }
             }
-            if (_defaultImage == null)
+
+            // Figure out which default we need
+            string type = (parameter as string)?.ToLowerInvariant() ?? "user";
+            switch (type)
             {
-                try
-                {
-                    string defaultFile = "DefaultUserPhoto.png";
-                    if (parameter != null)
-                    {
-                        string param = parameter.ToString();
-                        if (param.Equals("Tool", StringComparison.OrdinalIgnoreCase))
-                            defaultFile = "DefaultToolImage.png";
-                        else if (param.Equals("Logo", StringComparison.OrdinalIgnoreCase))
-                            defaultFile = "DefaultLogo.png";
-                    }
-                    var resourceUri = new Uri($"pack://application:,,,/Resources/{defaultFile}", UriKind.Absolute);
-                    _defaultImage = new BitmapImage(resourceUri);
-                }
-                catch
-                {
-                    _defaultImage = new BitmapImage();
-                }
+                case "tool":
+                    if (_defaultTool == null)
+                        _defaultTool = LoadFromResource("DefaultToolImage.png");
+                    return _defaultTool;
+
+                case "logo":
+                    if (_defaultLogo == null)
+                        _defaultLogo = LoadFromResource("DefaultLogo.png");
+                    return _defaultLogo;
+
+                default: // user
+                    if (_defaultUser == null)
+                        _defaultUser = LoadFromResource("DefaultUserPhoto.png");
+                    return _defaultUser;
             }
-            return _defaultImage;
         }
+
+        private BitmapImage LoadFromResource(string fileName)
+        {
+            try
+            {
+                var uri = new Uri($"pack://application:,,,/Resources/{fileName}", UriKind.Absolute);
+                return new BitmapImage(uri);
+            }
+            catch
+            {
+                return new BitmapImage(); // empty fallback
+            }
+        }
+
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
