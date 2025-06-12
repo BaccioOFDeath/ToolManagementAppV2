b9094304ff622f69f47bea2b9af5c0073804c5a9
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-06-12 23:57:02 +1200

    Convert CsvHelperUtilTests to xUnit

--- ToolManagementAppV2.Tests/CsvHelperUtilTests.cs
+++ ToolManagementAppV2.Tests/CsvHelperUtilTests.cs
@@ -1,13 +1,12 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 using System.Collections.Generic;
 using ToolManagementAppV2.Utilities.IO;
 
 namespace ToolManagementAppV2.Tests
 {
-    [TestClass]
     public class CsvHelperUtilTests
     {
-        [TestMethod]
+        [Fact]
         public void GetMapped_IgnoresHeaderCase()
         {
             var headers = new[] { "toolnumber", "location" };
@@ -22,11 +21,11 @@ namespace ToolManagementAppV2.Tests
                 .GetMethod("GetMapped", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)!
                 .Invoke(null, new object[] { row, headers, map, "Location" });
 
-            Assert.AreEqual("123", number);
-            Assert.AreEqual("Loc", location);
+            Assert.Equal("123", number);
+            Assert.Equal("Loc", location);
         }
 
-        [TestMethod]
+        [Fact]
         public void GetMapped_IgnoresHeaderCase_Reversed()
         {
             var headers = new[] { "TOOLNUMBER", "LOCATION" };
@@ -41,8 +40,8 @@ namespace ToolManagementAppV2.Tests
                 .GetMethod("GetMapped", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)!
                 .Invoke(null, new object[] { row, headers, map, "Location" });
 
-            Assert.AreEqual("321", number);
-            Assert.AreEqual("Loc", location);
+            Assert.Equal("321", number);
+            Assert.Equal("Loc", location);
         }
     }
 }
