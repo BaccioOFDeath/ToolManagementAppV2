8ee888a480188b595d9f6e835427b2c0d978abde
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-06-14 08:15:09 +1200

    Add converter Convert method tests

--- ToolManagementAppV2.Tests/Tests/NullToDefaultImageConverterTests.cs
+++ ToolManagementAppV2.Tests/Tests/NullToDefaultImageConverterTests.cs
@@ -3,12 +3,40 @@ using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 using ToolManagementAppV2.Utilities.Converters;
+using ToolManagementAppV2.Utilities.Helpers;
 using Xunit;
 
 namespace ToolManagementAppV2.Tests
 {
     public class NullToDefaultImageConverterTests
     {
+        [Fact]
+        public void Convert_Null_ReturnsDefaultUser()
+        {
+            var converter = new NullToDefaultImageConverter();
+            var result = converter.Convert(null, typeof(BitmapImage), null, CultureInfo.InvariantCulture);
+            var bmp = Assert.IsType<BitmapImage>(result);
+            Assert.Equal("pack://application:,,,/Resources/DefaultUserPhoto.png", bmp.UriSource.OriginalString);
+        }
+
+        [Fact]
+        public void Convert_ValidPath_ReturnsBitmapImage()
+        {
+            var converter = new NullToDefaultImageConverter();
+            var path = "Assets/Avatars/1.png";
+            var bmp = Assert.IsType<BitmapImage>(converter.Convert(path, typeof(BitmapImage), null, CultureInfo.InvariantCulture));
+            Assert.Equal(PathHelper.GetAbsolutePath(path), bmp.UriSource.OriginalString);
+        }
+
+        [Fact]
+        public void Convert_InvalidPath_ReturnsDefaultUser()
+        {
+            var converter = new NullToDefaultImageConverter();
+            var result = converter.Convert("invalid|path.png", typeof(BitmapImage), null, CultureInfo.InvariantCulture);
+            var bmp = Assert.IsType<BitmapImage>(result);
+            Assert.Equal("pack://application:,,,/Resources/DefaultUserPhoto.png", bmp.UriSource.OriginalString);
+        }
+
         [Fact]
         public void ConvertBack_BitmapImage_ReturnsUriString()
         {
