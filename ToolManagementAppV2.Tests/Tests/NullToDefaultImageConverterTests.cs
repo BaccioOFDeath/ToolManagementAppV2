4cf45b91d4f107221785aa02728e9b571faac1e8
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-06-13 21:52:19 +1200

    Implement ConvertBack in NullToDefaultImageConverter

---
+++ ToolManagementAppV2.Tests/Tests/NullToDefaultImageConverterTests.cs
@@ -0,0 +1,38 @@
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using ToolManagementAppV2.Utilities.Converters;
+using Xunit;
+
+namespace ToolManagementAppV2.Tests
+{
+    public class NullToDefaultImageConverterTests
+    {
+        [Fact]
+        public void ConvertBack_BitmapImage_ReturnsUriString()
+        {
+            var converter = new NullToDefaultImageConverter();
+            var uri = "pack://application:,,,/Resources/DefaultUserPhoto.png";
+            var bmp = new BitmapImage(new Uri(uri));
+            var result = converter.ConvertBack(bmp, typeof(string), null, CultureInfo.InvariantCulture);
+            Assert.Equal(uri, result);
+        }
+
+        [Fact]
+        public void ConvertBack_String_ReturnsSameString()
+        {
+            var converter = new NullToDefaultImageConverter();
+            var path = "image.png";
+            var result = converter.ConvertBack(path, typeof(string), null, CultureInfo.InvariantCulture);
+            Assert.Equal(path, result);
+        }
+
+        [Fact]
+        public void ConvertBack_InvalidInput_ReturnsBindingDoNothing()
+        {
+            var converter = new NullToDefaultImageConverter();
+            var result = converter.ConvertBack(42, typeof(string), null, CultureInfo.InvariantCulture);
+            Assert.Equal(Binding.DoNothing, result);
+        }
+    }
+}
