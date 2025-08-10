6eb95912a15f0929ca9aad2f6c1d6751a3199998
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-10 19:39:50 +1200

    test: add popup viewmodel command test

--- ToolManagementAppV2/ViewModels/RentToolPopupViewModel.cs
+++ ToolManagementAppV2/ViewModels/RentToolPopupViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.ObjectModel;
 using ToolManagementAppV2.Models.Domain;
@@ -8,23 +9,34 @@ namespace ToolManagementAppV2.ViewModels.Rental
     public class RentToolPopupViewModel : ObservableObject
     {
         public ObservableCollection<CustomerModel> Customers { get; }
-        public CustomerModel SelectedCustomer { get; set; }
+        public CustomerModel? SelectedCustomer { get; set; }
         public DateTime SelectedDueDate { get; set; } = DateTime.Today.AddDays(7);
-        public event EventHandler RequestClose;
+        public event EventHandler? RequestClose;
 
-        public CustomerModel SelectedCustomerResult { get; private set; }
+        public CustomerModel? SelectedCustomerResult { get; private set; }
         public DateTime SelectedDueDateResult { get; private set; }
+
+        public IRelayCommand CheckOutCommand { get; }
+        public IRelayCommand CancelCommand { get; }
 
         public RentToolPopupViewModel(ToolModel tool, IEnumerable<CustomerModel> customers)
         {
             Customers = new ObservableCollection<CustomerModel>(customers);
+            CheckOutCommand = new RelayCommand(Confirm);
+            CancelCommand = new RelayCommand(Cancel);
         }
 
-        public void Confirm()
+        void Confirm()
         {
             SelectedCustomerResult = SelectedCustomer;
             SelectedDueDateResult = SelectedDueDate;
             RequestClose?.Invoke(this, EventArgs.Empty);
         }
+
+        void Cancel()
+        {
+            RequestClose?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
+
