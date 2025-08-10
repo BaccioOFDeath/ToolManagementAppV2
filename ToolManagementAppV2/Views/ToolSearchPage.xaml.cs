40e0307cf082b11369d719a3ae5b1b703b7fcc8a
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-10 19:58:35 +1200

    Use VisualState setters for tile size

--- ToolManagementAppV2/Views/ToolSearchPage.xaml.cs
+++ ToolManagementAppV2/Views/ToolSearchPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ToolManagementAppV2.Views
@@ -7,6 +8,16 @@ namespace ToolManagementAppV2.Views
         public ToolSearchPage()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e) => UpdateState();
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e) => UpdateState();
+
+        private void UpdateState()
+        {
+            string state = ActualWidth < 800 ? "Narrow" : "Wide";
+            VisualStateManager.GoToState(this, state, true);
         }
     }
 }
