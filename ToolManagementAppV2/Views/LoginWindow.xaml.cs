ef43a1eaa9cea71db29bb9c84482c5b361478114
Garett Daly <garett@sdeuropean.co.nz>
2025-08-12 00:00:17 +1200

    coming along nicly

--- ToolManagementAppV2/Views/LoginWindow.xaml.cs
+++ ToolManagementAppV2/Views/LoginWindow.xaml.cs
@@ -1,3 +1,5 @@
+// Views/LoginWindow.xaml.cs
+using System;
 using System.Windows;
 using ToolManagementAppV2.ViewModels;
 
@@ -8,9 +10,18 @@ namespace ToolManagementAppV2
         public LoginWindow()
         {
             InitializeComponent();
-            var vm = new LoginViewModel();
-            vm.LoginSucceeded += (_, __) => DialogResult = true;
-            DataContext = vm;
+
+            if (DataContext == null)
+                DataContext = new LoginViewModel(); // optional: your App may set this already
+
+            if (DataContext is LoginViewModel vm)
+                vm.LoginSucceeded += OnLoginSucceeded;
+        }
+
+        private void OnLoginSucceeded(object? sender, EventArgs e)
+        {
+            DialogResult = true;
+            Close();
         }
     }
 }
