9ce402d47a657869ef82c4497597a5c5c0f464be
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-13 07:03:54 +1200

    Add image selection to tool editor

--- ToolManagementAppV2/ViewModels/ToolEditViewModel.cs
+++ ToolManagementAppV2/ViewModels/ToolEditViewModel.cs
@@ -2,21 +2,46 @@
 using System;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using ToolManagementAppV2.Interfaces;
 
 namespace ToolManagementAppV2.ViewModels
 {
     public class ToolEditViewModel : ObservableObject
     {
+        private readonly IFileDialogService _fileDialog;
+
         public ToolModel Tool { get; }
 
         public IRelayCommand SaveCommand { get; }
         public IRelayCommand CancelCommand { get; }
 
-        public ToolEditViewModel(ToolModel tool, Action onSave, Action onCancel)
+        public IRelayCommand BrowseImageCommand { get; }
+        public IRelayCommand RemoveImageCommand { get; }
+
+        public ToolEditViewModel(ToolModel tool, Action onSave, Action onCancel, IFileDialogService fileDialog)
         {
             Tool = tool;
+            _fileDialog = fileDialog;
+
             SaveCommand = new RelayCommand(onSave);
             CancelCommand = new RelayCommand(onCancel);
+
+            BrowseImageCommand = new RelayCommand(BrowseImage);
+            RemoveImageCommand = new RelayCommand(RemoveImage);
+        }
+
+        void BrowseImage()
+        {
+            var path = _fileDialog.OpenFile("Image Files|*.png;*.jpg;*.jpeg;*.bmp|All Files|*.*");
+            if (!string.IsNullOrEmpty(path))
+            {
+                Tool.ToolImagePath = path;
+            }
+        }
+
+        void RemoveImage()
+        {
+            Tool.ToolImagePath = string.Empty;
         }
     }
 }
