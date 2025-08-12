6feada748384d5108a9145b2e401e2866e628cab
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-13 10:28:39 +1200

    Add test for import mapping view model

---
+++ ToolManagementAppV2.Tests/ViewModels/ImportMappingViewModelTests.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using ToolManagementAppV2.ViewModels;
+using Xunit;
+
+namespace ToolManagementAppV2.Tests.ViewModels
+{
+    public class ImportMappingViewModelTests
+    {
+        [Fact]
+        public void Constructor_PopulatesMappingsWithProvidedLists()
+        {
+            var headers = new[] { "ToolNumber", "NameDescription", "SerialNumber" };
+            var properties = new[] { "ToolNumber", "NameDescription" };
+
+            var vm = new ImportMappingViewModel(headers, properties, () => { }, () => { });
+
+            Assert.Equal(headers, vm.ColumnHeaders);
+            Assert.Equal(properties, vm.Mappings.Select(m => m.PropertyName));
+            foreach (var mapping in vm.Mappings)
+            {
+                Assert.Equal(headers, mapping.AvailableColumns);
+            }
+        }
+    }
+}
