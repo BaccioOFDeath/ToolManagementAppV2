4712d9cabab7f6c34f79814c18d234fb58141463
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-12 20:09:52 +1200

    feat: add user search and editing commands

--- ToolManagementAppV2/Views/UsersEditWindow.xaml.cs
+++ ToolManagementAppV2/Views/UsersEditWindow.xaml.cs
@@ -1,13 +1,17 @@
-﻿// Views/UsersEditWindow.xaml.cs
+// Views/UsersEditWindow.xaml.cs
+using System;
 using System.Windows;
+using ToolManagementAppV2.Models.Domain;
+using ToolManagementAppV2.ViewModels;
 
 namespace ToolManagementAppV2.Views
 {
     public partial class UsersEditWindow : Window
     {
-        public UsersEditWindow()
+        public UsersEditWindow(User user, Action onSave, Action onCancel, Action onBrowseAvatar, Action onRemoveAvatar)
         {
             InitializeComponent();
+            DataContext = new UsersEditViewModel(user, onSave, onCancel, onBrowseAvatar, onRemoveAvatar);
         }
     }
 }
