424984c377d3c12f389ad09cefb93fb94b90b593
Garett Daly <garett@sdeuropean.co.nz>
2025-08-12 21:15:25 +1200

    added System.Windows.Data.Binding.DoNothing;

--- ToolManagementAppV2/Utilities/Converters/NullToBooleanConverter.cs
+++ ToolManagementAppV2/Utilities/Converters/NullToBooleanConverter.cs
@@ -35,7 +35,7 @@ namespace ToolManagementAppV2.Utilities.Converters
             {
                 Console.WriteLine(ex);
             }
-            return Binding.DoNothing;
+            return System.Windows.Data.Binding.DoNothing;
         }
     }
 }
