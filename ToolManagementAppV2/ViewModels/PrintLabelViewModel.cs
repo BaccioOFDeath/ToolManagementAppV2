514fe61c54d60c7058f12927e03394b897c7cbc7
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-14 20:46:40 +1200

    Handle print failures

--- ToolManagementAppV2/ViewModels/PrintLabelViewModel.cs
+++ ToolManagementAppV2/ViewModels/PrintLabelViewModel.cs
@@ -13,6 +13,7 @@ namespace ToolManagementAppV2.ViewModels
     {
         private readonly System.Action _closeAction;
         private readonly IDialogService _dialogService;
+        private readonly System.Action<FlowDocument> _printAction;
 
         public ObservableCollection<string> Templates { get; }
 
@@ -36,10 +37,16 @@ namespace ToolManagementAppV2.ViewModels
         public IRelayCommand PrintCommand { get; }
         public IRelayCommand CloseCommand { get; }
 
-        public PrintLabelViewModel(IDialogService dialogService, System.Action closeAction)
+        public PrintLabelViewModel(IDialogService dialogService, System.Action closeAction, System.Action<FlowDocument>? printAction = null)
         {
             _dialogService = dialogService;
             _closeAction = closeAction;
+            _printAction = printAction ?? (doc =>
+            {
+                var dlg = new System.Windows.Controls.PrintDialog();
+                if (dlg.ShowDialog() == true)
+                    dlg.PrintDocument(((IDocumentPaginatorSource)doc).DocumentPaginator, "Tool Labels");
+            });
             Templates = new ObservableCollection<string> { "Standard", "Compact" };
             _selectedTemplate = Templates.First();
             Items = new ObservableCollection<Tool>();
@@ -57,9 +64,14 @@ namespace ToolManagementAppV2.ViewModels
         private void Print()
         {
             var doc = BuildDocument();
-            var dlg = new System.Windows.Controls.PrintDialog();
-            if (dlg.ShowDialog() == true)
-                dlg.PrintDocument(((IDocumentPaginatorSource)doc).DocumentPaginator, "Tool Labels");
+            try
+            {
+                _printAction(doc);
+            }
+            catch (System.Exception ex)
+            {
+                _dialogService.ShowInfo($"Failed to print labels: {ex.Message}", "Print Labels");
+            }
         }
 
         private FlowDocument BuildDocument()
