1360c79f91dc2d45048b13981018b880aa5bb43b
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-17 20:01:33 +1200

    feat: add reusable search bar control

---
+++ ToolManagementAppV2/Views/Controls/SearchBar.xaml.cs
@@ -0,0 +1,33 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ToolManagementAppV2.Controls
+{
+    public partial class SearchBar : UserControl
+    {
+        public SearchBar()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(SearchBar),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public ICommand? SearchCommand
+        {
+            get => (ICommand?)GetValue(SearchCommandProperty);
+            set => SetValue(SearchCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty SearchCommandProperty =
+            DependencyProperty.Register(nameof(SearchCommand), typeof(ICommand), typeof(SearchBar));
+    }
+}
