487dd20da07188baa48f61d46b87a86e2c893cc6
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-23 22:08:22 +1200

    Mark DTO string properties nullable

--- InventoryManagementApp/Models/DTOs/ItemImportDto.cs
+++ InventoryManagementApp/Models/DTOs/ItemImportDto.cs
@@ -2,15 +2,15 @@
 {
     public class ItemImportDto
     {
-        public string ItemNumber { get; set; }
-        public string Name { get; set; }
-        public string Location { get; set; }
-        public string Brand { get; set; }
-        public string PartNumber { get; set; }
-        public string Supplier { get; set; }
+        public string? ItemNumber { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public string? Brand { get; set; }
+        public string? PartNumber { get; set; }
+        public string? Supplier { get; set; }
         public DateTime? PurchasedDate { get; set; }
-        public string Notes { get; set; }
-        public string Keywords { get; set; }
+        public string? Notes { get; set; }
+        public string? Keywords { get; set; }
         public int AvailableQuantity { get; set; }
         public int RentedQuantity { get; set; }
     }
