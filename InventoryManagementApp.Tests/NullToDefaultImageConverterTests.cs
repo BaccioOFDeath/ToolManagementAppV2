cf58bcb23ed9f66387c08a4ed988b9bf8129f483
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-24 11:23:28 +1200

    Cache invalid image paths

--- InventoryManagementApp.Tests/NullToDefaultImageConverterTests.cs
+++ InventoryManagementApp.Tests/NullToDefaultImageConverterTests.cs
@@ -5,6 +5,8 @@ using System.Windows;
 using System.Windows.Media.Imaging;
 using InventoryManagementApp.Utilities.Converters;
 using InventoryManagementApp.Utilities.Helpers;
+using System.Collections.Concurrent;
+using System.Reflection;
 using Xunit;
 
 namespace InventoryManagementApp.Tests
@@ -24,6 +26,38 @@ namespace InventoryManagementApp.Tests
                     var defaultImage = Assert.IsType<BitmapImage>(converter.Convert(null, typeof(BitmapImage), "user", CultureInfo.InvariantCulture));
                     var result = Assert.IsType<BitmapImage>(converter.Convert("../nonexistent.png", typeof(BitmapImage), "user", CultureInfo.InvariantCulture));
                     Assert.Same(defaultImage, result);
+                }
+                catch (Exception ex)
+                {
+                    threadEx = ex;
+                }
+                finally
+                {
+                    Application.Current?.Shutdown();
+                }
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+            if (threadEx != null) throw threadEx;
+        }
+
+        [Fact]
+        public void Convert_InvalidPath_IsCached()
+        {
+            Exception? threadEx = null;
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    var converter = new NullToDefaultImageConverter();
+                    var field = typeof(NullToDefaultImageConverter).GetField("_invalidPaths", BindingFlags.NonPublic | BindingFlags.Static);
+                    var cache = (ConcurrentDictionary<string, byte>)field!.GetValue(null)!;
+                    cache.Clear();
+                    var path = "../nonexistent.png";
+                    Assert.Null(PathHelper.GetAbsolutePath(path, false));
+                    converter.Convert(path, typeof(BitmapImage), "user", CultureInfo.InvariantCulture);
+                    Assert.True(cache.ContainsKey(path));
                 }
                 catch (Exception ex)
                 {
