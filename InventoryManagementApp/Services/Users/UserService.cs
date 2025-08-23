ca3e9c4d05623a356cab425eb4aa7f11b4bdd72c
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-23 22:33:53 +1200

    Replace Sqlite error code comparisons

--- InventoryManagementApp/Services/Users/UserService.cs
+++ InventoryManagementApp/Services/Users/UserService.cs
@@ -230,7 +230,7 @@ namespace InventoryManagementApp.Services.Users
             {
                 user.UserID = Convert.ToInt32(await cmd.ExecuteScalarAsync());
             }
-            catch (SqliteException ex) when (ex.SqliteErrorCode == (int)SqliteErrorCode.Constraint &&
+            catch (SqliteException ex) when (ex.SqliteErrorCode == SQLitePCL.raw.SQLITE_CONSTRAINT &&
                                              ex.Message.Contains("Users.UserName", StringComparison.OrdinalIgnoreCase))
             {
                 throw new InvalidOperationException("A user with the same username already exists.", ex);
