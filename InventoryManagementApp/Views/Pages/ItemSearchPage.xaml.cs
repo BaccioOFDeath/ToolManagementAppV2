e97371aac4a8a94139517213ccde3b09e51b371e
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-08-24 15:25:59 +1200

    Add search and category filtering to item search page

--- InventoryManagementApp/Views/Pages/ItemSearchPage.xaml.cs
+++ InventoryManagementApp/Views/Pages/ItemSearchPage.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using InventoryManagementApp.ViewModels;
 
 namespace InventoryManagementApp.Views.Pages
 {
@@ -10,7 +11,12 @@ namespace InventoryManagementApp.Views.Pages
             InitializeComponent();
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e) => UpdateState();
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateState();
+            if (DataContext is ItemManagementViewModel vm)
+                await vm.SearchCommand.ExecuteAsync(null);
+        }
 
         private void Page_SizeChanged(object sender, SizeChangedEventArgs e) => UpdateState();
 
