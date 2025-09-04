10dadf65d6435c544ff59e62e26d657d3e38cbc4
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2025-09-04 20:44:04 +1200

    Remove binary image resources from DeviceManagementApp

---
+++ DeviceManagementApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace DeviceManagementApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
