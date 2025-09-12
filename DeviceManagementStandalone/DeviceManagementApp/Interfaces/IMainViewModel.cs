2a0dae773910c4d9880165a49a1f85ab0d2e9a9d
Garett Daly <garett@sdeuropean.co.nz>
2025-09-12 21:26:22 +1200

    remove device app from folder

--- DeviceManagementStandalone/DeviceManagementApp/Interfaces/IMainViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace DeviceManagementApp.Interfaces
-{
-    /// <summary>
-    /// Marker interface for the main view model.
-    /// </summary>
-    public interface IMainViewModel { }
-}
--- DeviceManagementStandalone/DeviceManagementApp/Interfaces/INavigationService.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Controls;
-
-namespace DeviceManagementApp.Interfaces
-{
-    public interface INavigationService
-    {
-        void Navigate(Page page);
-    }
-}
