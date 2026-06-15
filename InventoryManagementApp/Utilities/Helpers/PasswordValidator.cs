f69a638770b59760347dab111a002f076d752569
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2026-06-15 17:19:58 +1200

    Enforce password complexity rules

--- InventoryManagementApp/Utilities/Helpers/PasswordValidator.cs
+++ InventoryManagementApp/Utilities/Helpers/PasswordValidator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace InventoryManagementApp.Utilities.Helpers
 {
@@ -10,6 +11,30 @@ namespace InventoryManagementApp.Utilities.Helpers
             if (string.IsNullOrWhiteSpace(password))
             {
                 error = "Password cannot be empty.";
+                return false;
+            }
+
+            if (password.Length < 8)
+            {
+                error = "Password must be at least 8 characters long.";
+                return false;
+            }
+
+            if (!password.Any(char.IsUpper))
+            {
+                error = "Password must contain at least one uppercase letter.";
+                return false;
+            }
+
+            if (!password.Any(char.IsLower))
+            {
+                error = "Password must contain at least one lowercase letter.";
+                return false;
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                error = "Password must contain at least one digit.";
                 return false;
             }
 
