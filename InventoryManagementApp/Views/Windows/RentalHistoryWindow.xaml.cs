e9cfc454b1fc53f2a34e48bcf904f7533da0dc28
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2026-06-16 08:23:05 +1200

    Enable rental history row drilldown

--- InventoryManagementApp/Views/Windows/RentalHistoryWindow.xaml.cs
+++ InventoryManagementApp/Views/Windows/RentalHistoryWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using InventoryManagementApp.ViewModels.Rental;
 using InventoryManagementApp.Utilities.Extensions;
 
@@ -15,6 +17,23 @@ namespace InventoryManagementApp.Views.Windows
         public RentalHistoryWindow(RentalHistoryViewModel viewModel) : this()
         {
             DataContext = viewModel;
+        }
+
+        private void HistoryRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is RentalHistoryViewModel vm && vm.OpenDetailsCommand.CanExecute(null))
+            {
+                vm.OpenDetailsCommand.Execute(null);
+            }
+        }
+
+        private void HistoryRow_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is DataGridRow row && !row.IsSelected)
+            {
+                row.IsSelected = true;
+                e.Handled = true;
+            }
         }
     }
 }
