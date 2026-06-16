e6893af7e79247ebcc158abc8c0c98e9021678ab
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2026-06-16 14:20:26 +1200

    Tighten rentals desktop workflow interactions
    Add faster keyboard and context-menu paths to the rentals workstation and surface rental document print actions.

--- InventoryManagementApp/Views/Pages/ManageRentalsPage.xaml.cs
+++ InventoryManagementApp/Views/Pages/ManageRentalsPage.xaml.cs
@@ -1,7 +1,10 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using InventoryManagementApp.Models.Domain;
 using InventoryManagementApp.ViewModels;
+using RentalModel = InventoryManagementApp.Models.Domain.Rental;
 
 namespace InventoryManagementApp.Views.Pages
 {
@@ -14,14 +17,19 @@ namespace InventoryManagementApp.Views.Pages
         {
             InitializeComponent();
             Loaded += ManageRentalsPage_Loaded;
+            PreviewKeyDown += ManageRentalsPage_PreviewKeyDown;
         }
 
         private async void ManageRentalsPage_Loaded(object sender, RoutedEventArgs e)
         {
+            Focus();
             if (DataContext is ManageRentalsViewModel vm)
             {
                 await vm.LoadRentalsAsync();
             }
+
+            SearchTextBox.Focus();
+            SearchTextBox.SelectAll();
         }
 
         private void RentalRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -50,13 +58,135 @@ namespace InventoryManagementApp.Views.Pages
             SelectRowForContextMenu(sender, e);
         }
 
-        private static void SelectRowForContextMenu(object sender, MouseButtonEventArgs e)
+        private void ManageRentalsPage_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (sender is DataGridRow row && !row.IsSelected)
+            if (DataContext is not ManageRentalsViewModel vm)
+                return;
+
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.F)
             {
-                row.IsSelected = true;
+                SearchTextBox.Focus();
+                SearchTextBox.SelectAll();
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.P)
+            {
+                vm.PrintSearchResultsCommand.Execute(null);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == (ModifierKeys.Control | ModifierKeys.Shift) && e.Key == Key.P)
+            {
+                vm.PrintCheckedOutCommand.Execute(null);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == (ModifierKeys.Control | ModifierKeys.Shift) && e.Key == Key.R)
+            {
+                vm.PrintRequestsCommand.Execute(null);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.D)
+            {
+                OpenFocusedDetails(vm);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.H && vm.OpenHistoryCommand.CanExecute(null))
+            {
+                vm.OpenHistoryCommand.Execute(null);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.I && vm.CheckInCommand.CanExecute(null))
+            {
+                vm.CheckInCommand.Execute(null);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.E && vm.ExtendCommand.CanExecute(null))
+            {
+                vm.ExtendCommand.Execute(null);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.R && vm.PlaceRequestCommand.CanExecute(null))
+            {
+                vm.PlaceRequestCommand.Execute(null);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.None && e.Key == Key.Enter)
+            {
+                OpenFocusedDetails(vm);
+                e.Handled = true;
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.None && e.Key == Key.Delete && vm.DeleteRentalCommand.CanExecute(null))
+            {
+                vm.DeleteRentalCommand.Execute(null);
                 e.Handled = true;
             }
+        }
+
+        private static void OpenFocusedDetails(ManageRentalsViewModel vm)
+        {
+            if (Keyboard.FocusedElement is DependencyObject focusedElement && FindAncestor<DataGrid>(focusedElement) is DataGrid grid)
+            {
+                if (grid.SelectedItem is Reservation && vm.OpenRequestDetailsCommand.CanExecute(null))
+                {
+                    vm.OpenRequestDetailsCommand.Execute(null);
+                    return;
+                }
+            }
+
+            if (vm.OpenRentalDetailsCommand.CanExecute(null))
+                vm.OpenRentalDetailsCommand.Execute(null);
+            else if (vm.OpenRequestDetailsCommand.CanExecute(null))
+                vm.OpenRequestDetailsCommand.Execute(null);
+        }
+
+        private void SelectRowForContextMenu(object sender, MouseButtonEventArgs e)
+        {
+            var row = sender as DataGridRow ?? FindAncestor<DataGridRow>(e.OriginalSource as DependencyObject);
+            if (row == null)
+                return;
+
+            row.IsSelected = true;
+            row.Focus();
+
+            if (DataContext is ManageRentalsViewModel vm)
+            {
+                if (row.Item is RentalModel rental)
+                    vm.SelectedRental = rental;
+                else if (row.Item is Reservation request)
+                    vm.SelectedRequest = request;
+            }
+        }
+
+        private static T? FindAncestor<T>(DependencyObject? current) where T : DependencyObject
+        {
+            while (current != null)
+            {
+                if (current is T match)
+                    return match;
+
+                current = VisualTreeHelper.GetParent(current);
+            }
+
+            return null;
         }
     }
 }
