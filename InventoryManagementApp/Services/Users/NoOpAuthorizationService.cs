566632974714e77dc43fb9237f7e03817721dfcf
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2026-06-17 14:17:23 +1200

    Add permission-specific authorization checks

--- InventoryManagementApp/Services/Users/NoOpAuthorizationService.cs
+++ InventoryManagementApp/Services/Users/NoOpAuthorizationService.cs
@@ -5,6 +5,10 @@ namespace InventoryManagementApp.Services.Users
     public class NoOpAuthorizationService : IAuthorizationService
     {
         public bool IsAdmin => true;
+        public bool HasPermission(string permissionKey) => true;
+        public bool HasAnyPermission(params string[] permissionKeys) => true;
         public void EnsureAdmin() { }
+        public void EnsurePermission(string permissionKey) { }
+        public void EnsureAnyPermission(params string[] permissionKeys) { }
     }
 }
