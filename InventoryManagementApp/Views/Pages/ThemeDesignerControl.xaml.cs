03f362d9fe0d4aa681ebdba100721a5e8aa91096
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2026-06-19 10:26:34 +1200

    Add theme designer control code-behind

---
+++ InventoryManagementApp/Views/Pages/ThemeDesignerControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace InventoryManagementApp.Views.Pages
+{
+    public partial class ThemeDesignerControl : UserControl
+    {
+        public ThemeDesignerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
