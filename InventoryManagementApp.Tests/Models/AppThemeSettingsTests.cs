d3c35aaeb5d623b94aa0e6c9604f8b133dbb7278
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2026-06-19 14:25:14 +1200

    Fix theme settings test namespace import

--- InventoryManagementApp.Tests/Models/AppThemeSettingsTests.cs
+++ InventoryManagementApp.Tests/Models/AppThemeSettingsTests.cs
@@ -1,5 +1,6 @@
 using InventoryManagementApp.Interfaces;
 using InventoryManagementApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
