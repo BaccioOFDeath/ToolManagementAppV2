ca15b08a91354c14326650035ab96b09d554e1f7
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2026-06-28 19:16:50 +1200

    Remove legacy activity log result data property

--- InventoryManagementApp/Models/Result.cs
+++ InventoryManagementApp/Models/Result.cs
@@ -1,12 +1,7 @@
-using InventoryManagementApp.Models.Domain;
-
 namespace InventoryManagementApp.Models
 {
     public record Result(bool Success, string? ErrorMessage = null);
 
     public record Result<T>(T? Value, bool Success, string? ErrorMessage = null)
-        : Result(Success, ErrorMessage)
-    {
-        public List<ActivityLog>? Data { get; internal set; }
-    }
+        : Result(Success, ErrorMessage);
 }
