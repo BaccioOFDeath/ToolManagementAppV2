453aa0383d8762a86a23cbbbcda30576eae9de5d
Garett <101615652+BaccioOFDeath@users.noreply.github.com>
2026-07-07 01:27:58 +1200

    Cover user editor save state

--- InventoryManagementApp.Tests/UsersEditViewModelTests.cs
+++ InventoryManagementApp.Tests/UsersEditViewModelTests.cs
@@ -22,9 +22,71 @@ namespace InventoryManagementApp.Tests
         }
 
         [Fact]
+        public void SaveCommand_RequiresUsernameAndRefreshesReadiness()
+        {
+            var user = new User { UserName = "   " };
+            var dialog = new DummyFileDialogService();
+            var vm = new UsersEditViewModel(user, dialog, onSave: () => Task.CompletedTask, onCancel: () => { });
+
+            Assert.False(vm.CanSaveUserProfile);
+            Assert.False(vm.SaveCommand.CanExecute(null));
+            Assert.Contains("Enter a username", vm.SaveStatusText);
+
+            user.UserName = "workshop-manager";
+
+            Assert.True(vm.CanSaveUserProfile);
+            Assert.True(vm.SaveCommand.CanExecute(null));
+            Assert.Contains("User profile ready", vm.SaveStatusText);
+        }
+
+        [Fact]
+        public async Task SaveCommand_ExposesBusyStateAndDisablesEditorActionsWhileRunning()
+        {
+            var user = new User { UserName = "workshop-manager" };
+            var dialog = new DummyFileDialogService();
+            var saveStarted = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+            var releaseSave = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+            var saveCalls = 0;
+            var vm = new UsersEditViewModel(user, dialog, onSave: async () =>
+            {
+                saveCalls++;
+                saveStarted.SetResult();
+                await releaseSave.Task;
+            }, onCancel: () => { });
+
+            var saveTask = vm.SaveCommand.ExecuteAsync(null);
+            await saveStarted.Task;
+
+            Assert.True(vm.IsSaving);
+            Assert.False(vm.CanEditProfile);
+            Assert.False(vm.CanSaveUserProfile);
+            Assert.False(vm.SaveCommand.CanExecute(null));
+            Assert.False(vm.CancelCommand.CanExecute(null));
+            Assert.False(vm.BrowseImageCommand.CanExecute(null));
+            Assert.False(vm.RemoveImageCommand.CanExecute(null));
+            Assert.False(vm.SelectAdvisorPresetCommand.CanExecute(null));
+            Assert.False(vm.SelectTechnicianPresetCommand.CanExecute(null));
+            Assert.False(vm.SelectAdminPresetCommand.CanExecute(null));
+            Assert.False(vm.ClearPermissionsCommand.CanExecute(null));
+            Assert.Contains("Saving user profile", vm.SaveStatusText);
+            Assert.Equal(1, saveCalls);
+
+            releaseSave.SetResult();
+            await saveTask;
+
+            Assert.False(vm.IsSaving);
+            Assert.True(vm.CanEditProfile);
+            Assert.True(vm.SaveCommand.CanExecute(null));
+            Assert.True(vm.CancelCommand.CanExecute(null));
+            Assert.True(vm.BrowseImageCommand.CanExecute(null));
+            Assert.Contains("User profile ready", vm.SaveStatusText);
+            Assert.Equal(1, saveCalls);
+        }
+
+        [Fact]
         public void BrowseImageCommand_UsesRelativePathForBaseDirectoryFile()
         {
-            var user = new User();
+            var user = new User { UserName = "photo-user" };
             var dialog = new DummyFileDialogService();
             var baseDir = AppDomain.CurrentDomain.BaseDirectory;
             var filePath = Path.Combine(baseDir, Guid.NewGuid() + ".png");
@@ -44,7 +106,7 @@ namespace InventoryManagementApp.Tests
         [Fact]
         public void BrowseImageCommand_CopiesExternalFileToAssets()
         {
-            var user = new User();
+            var user = new User { UserName = "photo-user" };
             var dialog = new DummyFileDialogService();
             var tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".png");
             File.WriteAllText(tempFile, "test");
@@ -53,7 +115,6 @@ namespace InventoryManagementApp.Tests
 
             vm.BrowseImageCommand.Execute(null);
 
-            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
             var destPath = AppAssetHelper.ResolveAssetPath(user.UserPhotoPath);
 
             Assert.StartsWith(Path.Combine("Assets", "UserPhotos"), user.UserPhotoPath);
@@ -67,7 +128,7 @@ namespace InventoryManagementApp.Tests
         [Fact]
         public void RemoveImageCommand_ClearsPhotoPath()
         {
-            var user = new User { UserPhotoPath = "test.png" };
+            var user = new User { UserName = "photo-user", UserPhotoPath = "test.png" };
             var dialog = new DummyFileDialogService();
             var vm = new UsersEditViewModel(user, dialog, onSave: () => Task.CompletedTask, onCancel: () => { });
 
@@ -79,7 +140,7 @@ namespace InventoryManagementApp.Tests
         [Fact]
         public void RemoveImageCommand_SetsInitialsBrushToVisibleBrush()
         {
-            var user = new User { UserPhotoPath = "test.png", InitialsBrush = Brushes.Transparent };
+            var user = new User { UserName = "photo-user", UserPhotoPath = "test.png", InitialsBrush = Brushes.Transparent };
             var dialog = new DummyFileDialogService();
             var vm = new UsersEditViewModel(user, dialog, onSave: () => Task.CompletedTask, onCancel: () => { });
 
